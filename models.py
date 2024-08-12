c9f6668f50e48b3818c84fcb5f0efe4d9683612a
tasnimhassan <161735551+tasnimhassan@users.noreply.github.com>
2024-08-13 01:42:54 +0300

    Update models.py

--- models.py
+++ models.py
@@ -55,34 +55,3 @@ class contact(db.Model):
 
 
 
-# from flask_sqlalchemy import SQLAlchemy
-
-# db= SQLAlchemy()
-
-# class panier(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     produit_id = db.Column(db.Integer, nullable=False)
-#     quantite = db.Column(db.Integer, nullable=False)
-
-# class paiment(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     first_name = db.Column(db.String(100), nullable=False)
-#     last_name = db.Column(db.String(100), nullable=False)
-#     card_name = db.Column(db.String(100), nullable=False)
-#     card_number = db.Column(db.String(16), nullable=False)
-#     expiry_date = db.Column(db.String(7), nullable=False)
-#     cvv = db.Column(db.String(4), nullable=False)
-#     address = db.Column(db.String(255), nullable=False)
-
-#     def __repr__(self):
-#         return f'<Paiment {self.id}>'
-
-# class contact(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     phone_number = db.Column(db.String(20), nullable=False)
-#     full_name = db.Column(db.String(100), nullable=False)
-#     email = db.Column(db.String(120), nullable=False)
-#     message = db.Column(db.Text, nullable=False)
-
-#     def __repr__(self):
-#         return f'<contact {self.id}>'   
