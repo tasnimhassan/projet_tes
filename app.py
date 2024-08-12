413d8d94d12df47e225bcf05ae013238fe945eaa
tasnimhassan <161735551+tasnimhassan@users.noreply.github.com>
2024-08-13 01:42:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,20 +15,3 @@ with app.app_context():
 if __name__ == '__main':
     app.run(debug=True)
 
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-
-# # Configuration de la base de données (exemple avec SQLite)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
-# db = SQLAlchemy(app)
-
-# @app.route('/')
-# def hello_worl  d():
-#     return 'Hello, World!'
-
-# if __name__ == '__main__':
-#   app.run(debug=True)
-
