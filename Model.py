ad4f74f03ad6f3f38d481909dbc799ef86a806a0
hassan <nimah5828gmail.com>
2024-09-04 12:32:04 +0300

    first commit

--- Model.py
+++ Model.py
@@ -68,7 +68,14 @@ class Contact(db.Model):
         self.email = email
         self.message = message
 
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    email=db.Column(db.String(120), nullable=False)
+    password=db.Column(db.String(10), nullable=False)
+    
 
+
+      
 
 
    
--- routes.py
+++ routes.py
@@ -5,12 +5,14 @@ from flask import Flask, request, jsonify, redirect, url_for, render_template,fl
 from Model import Panier
 from Model import Paiment
 from Model import Contact
+from Model import User
 
 
 
 from app import *
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'your_secret_key'
 db.init_app(app)
 with app.app_context():
@@ -26,13 +28,23 @@ with app.app_context():
 
 
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
 @app.route('/produit')
 def produit():
-    return render_template('produit.html')    
+    return render_template('produit.html')   
+
+
+# @app.route('/inscription')
+# def inscription():
+#     return render_template('inscription.html')
+
+
+# @app.route('/login')
+# def login():
+#     return render_template('login.html')         
 
 
 @app.route('/panier', methods=['GET', 'POST'])
@@ -102,6 +114,59 @@ def paiment():
         return redirect(url_for('paiment'))
     
     return render_template('paiment.html')    
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method=="POST":
+        email=request.form['email']
+        password=request.form['password']
+        
+        # Récupérer l'utilisateur correspondant à l'email fourni
+        user = User.query.filter_by(email=email).first()
+        if user:
+            # Vérifier si le mot de passe correspond
+            if user.password== password:
+                flash('Vous êtes connecté!', 'success')
+                 # Stocker l'ID de l'utilisateur dans la session
+                session['user_id'] = user.id
+                return redirect(url_for('index'))
+            else:
+                flash('Mot de passe incorrect!', 'error')
+        else:
+           
+            flash('Utilisateur non trouvé!', 'error')       
+    return render_template('login.html', title='Login')
+    
+    
+
+
+@app.route('/', methods=['GET', 'POST'])
+def inscription():
+    if request.method=='POST':
+        email=request.form['email']
+        password=request.form['password']
+        rePassword=request.form['rePassword']
+        if password != rePassword:
+            flash('le deux mot de passe ne sont pas le meme', 'error')
+            return redirect(url_for('inscription'))
+        else:
+            
+            newUser=User(email=email, password=password)
+            db.session.add(newUser)
+            db.session.commit()
+            flash("Brovo! vous etes bien Inscrit", "success")
+            return redirect(url_for('login'))
+    return render_template('inscription.html', title='inscription')
+
+@app.route('/sedeconnecter')
+def deconnecter():
+    
+    # Supprimer l'ID de l'utilisateur de la session
+    session.pop('user_id', None)
+    flash('Vous êtes déconnecté!', 'success')
+    return redirect(url_for('login'))
+            
 
 
 
